da750ea143f1d76d0e97bbcfa452b4c980b5699d
i-ka <i-ka2010@mail.ru>
2018-04-08 06:29:44 +0500

    banners 2

---
+++ Assets/Scripts/TribuneBannerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TribuneBannerController : MonoBehaviour {
+
+	public List<Transform> banners = new List<Transform>();
+	void Start () {
+		var selectedBanners = new Transform[2];
+		selectedBanners[0] = banners[Random.Range(0, banners.Count)];
+		selectedBanners[1] = banners[Random.Range(0, banners.Count)];
+
+		Instantiate(selectedBanners[0], transform, false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
